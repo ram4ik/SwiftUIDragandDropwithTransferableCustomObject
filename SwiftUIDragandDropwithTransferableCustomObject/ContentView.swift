5e69149c0baeb4795302b45d1330df0edbdc00dc
Ramill <ramill_ibragimov@icloud.com>
2023-07-24 11:32:29 +0300

    Initial Commit

---
+++ SwiftUIDragandDropwithTransferableCustomObject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIDragandDropwithTransferableCustomObject
+//
+//  Created by Ramill Ibragimov on 24.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDragandDropwithTransferableCustomObject/SwiftUIDragandDropwithTransferableCustomObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDragandDropwithTransferableCustomObjectApp.swift
+//  SwiftUIDragandDropwithTransferableCustomObject
+//
+//  Created by Ramill Ibragimov on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDragandDropwithTransferableCustomObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
